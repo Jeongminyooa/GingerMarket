81df22d1d5053a636815f985ff929a7a3b5f25fe
lorlorv <dev.eunseo@gmail.com>
2022-05-26 21:41:30 +0900

    #21 Docs : DB 연결 & JPA 설정

--- src/main/java/com/ssd/gingermarket/GingerMarketApplication.java
+++ src/main/java/com/ssd/gingermarket/GingerMarketApplication.java
@@ -2,9 +2,10 @@ package com.ssd.gingermarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class GingerMarketApplication {
 
 	public static void main(String[] args) {
