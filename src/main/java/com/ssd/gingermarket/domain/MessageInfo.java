1c312db4b8bf67f62a175d0d33f264ef74fe3b32
lorlorv <dev.eunseo@gmail.com>
2022-06-19 17:31:54 +0900

    #31 Feat : Message Entity

--- src/main/java/com/ssd/gingermarket/domain/MessageInfo.java
+++ src/main/java/com/ssd/gingermarket/domain/MessageInfo.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -20,39 +22,38 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Getter
-@DynamicInsert //null인 필드값이 insert 시 제외되게하며, default값을 넣어주기 위함 
-@Table
-public class Message extends BaseTime {
+@Table(name="messageinfo")
+public class MessageInfo extends BaseTime {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MessageSequence")
-	@SequenceGenerator(sequenceName = "MessageSequence", name = "MessageSequenceGenerator", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MessageInfoSequence")
+	@SequenceGenerator(sequenceName = "MessageInfoSequence", name = "MessageInfoSequenceGenerator", allocationSize = 1)
 	@Column(unique=true)
 	private Long messageIdx;
+	
+	@ManyToOne
+	@JoinColumn(name = "room_idx")
+	private MessageRoom room;
 	
 	@Column(nullable = false)
 	private String content;
 	
-	@Column(length = 3, nullable = false)
-	@ColumnDefault("'N'")
+	@Column(length = 1)
 	private String isRead;
 	
-	@ManyToOne
-	@JoinColumn(name = "post_idx")
-	private SharePost post;
-	
-	@Column(nullable = false)
+	@Column(nullable = false) //추후 User 참조
 	private Long senderIdx;
-
-	@Column(length = 3, nullable = false)
-	@ColumnDefault("'N'")
-	private String isSender;
 	
+	
+	@PrePersist
+	public void prePersist(){
+		this.isRead = this.isRead == null ? "N" : this.isRead;
+	}
 	
 	public void updateReadOrNot(boolean isRead) {
 		if(isRead)
-			this.isRead = "'Y'";
+			this.isRead = "Y";
 		else
-			this.isRead = "'N'";
+			this.isRead = "N";
 	}
 	
 }
