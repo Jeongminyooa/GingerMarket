aaffefc732e059ed20794cdee48ca8806ab3b204
Jeongminyooa <jmjmjmz732002@naver.com>
2022-06-22 00:06:54 +0900

    #35 Feat : 공동구매 게시글 히스토리 조회 API

--- src/main/java/com/ssd/gingermarket/controller/mypage/GetHistoryOfGroupController.java
+++ src/main/java/com/ssd/gingermarket/controller/mypage/GetHistoryOfGroupController.java
@@ -3,6 +3,7 @@ package com.ssd.gingermarket.controller.mypage;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ssd.gingermarket.dto.GroupBuyingDto;
+import com.ssd.gingermarket.service.ApplyInfoService;
 import com.ssd.gingermarket.service.GroupBuyingService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,29 +22,32 @@ import lombok.RequiredArgsConstructor;
 public class GetHistoryOfGroupController {
 	
 	private final GroupBuyingService groupBuyingService;
+	private final ApplyInfoService applyInfoService;
 	
 	@GetMapping("/group")
 	public String getGroupHistory (Model model,
-			HttpServletRequest req) {
+			HttpServletRequest request) {
 		
-		long userIdx = 1;
+		HttpSession session = request.getSession();
+	    long userIdx = (long) session.getAttribute("userIdx");
 		
-		List<GroupBuyingDto.MyPageInfo> list = groupBuyingService.getGroupBuyingByUserId(userIdx);
+		List<GroupBuyingDto.MyPageInfo> list = groupBuyingService.getGroupBuyingByUserIdx(userIdx);
 		
-		// model.addAttribute("groupList", list);
+		model.addAttribute("groupList", list);
 		
 		return "content/mypage/mypage_groupList :: #listContent";
 	}
 	
 	@GetMapping("/apply")
 	public String getSharePostByMessage (Model model,
-			HttpServletRequest req) {
+			HttpServletRequest request) {
 		
-		long userIdx = 1;
+		HttpSession session = request.getSession();
+	    long userIdx = (long) session.getAttribute("userIdx");
 		
-		// List<GroupBuyingDto.MyPageInfo> list = applyService.getAllApplyByAuthor(userIdx);
+		List<GroupBuyingDto.MyPageInfo> list = applyInfoService.getAllApplyByAuthor(userIdx);
 		
-		// model.addAttribute("groupList", list);
+		model.addAttribute("groupList", list);
 		
 		return "content/mypage/mypage_groupList :: #listContent";
 	}
--- src/main/java/com/ssd/gingermarket/service/GroupBuyingService.java
+++ src/main/java/com/ssd/gingermarket/service/GroupBuyingService.java
@@ -26,7 +26,7 @@ public interface GroupBuyingService {
 	public List<GroupBuyingDto.DetailResponse> getFavPost(Long userIdx);
   
 	// 사용자가 작성한 공구 포스트 리스트
-	public List<GroupBuyingDto.MyPageInfo> getGroupBuyingByUserId(Long userIdx);
+	public List<GroupBuyingDto.MyPageInfo> getGroupBuyingByUserIdx(Long userIdx);
 	
 	// 공구 포스트 상세 조회
 	public GroupBuyingDto.DetailResponse getPost(Long groupIdx);
